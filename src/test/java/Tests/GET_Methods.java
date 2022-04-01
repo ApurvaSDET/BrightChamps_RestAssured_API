49d5d756a00fce4bbc3868b02c4aed1adc9d31e1
apurvakushwaha <ishu.kush@gmail.com>
2022-04-01 19:21:28 +0530

    Grouping of Test Cases

--- src/test/java/Tests/GET_Methods.java
+++ src/test/java/Tests/GET_Methods.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class GET_MethodsTest {
+public class GET_Methods {
 
     private GET_Method_Helper GET_Method_Helper;
 
