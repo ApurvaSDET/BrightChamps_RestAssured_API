06f50dd30cf1141ad46e171e1817ac488d7332dd
apurvakushwaha <ishu.kush@gmail.com>
2022-04-01 17:19:50 +0530

    Fixing Surefire mvn issue

--- src/main/java/Utils/ConfigManager.java
+++ src/main/java/Utils/ConfigManager.java
@@ -71,7 +71,7 @@ public class ConfigManager {
         }
         try {
             Connection dbConnection = DriverManager
-                    .getConnection(System.getenv("DB_CONNECTIONURL")+"/?autoReconnect=true&failOverReadOnly=false&maxReconnects=10",System.getenv("DB_USERNAME"),System.getenv("DB_PASSWORD"));
+                    .getConnection(System.getenv("DB_CONNECTIONURL")+"/?enabledTLSProtocols=TLSv1.2",System.getenv("DB_USERNAME"),System.getenv("DB_PASSWORD"));
 
             Statement stmt = dbConnection.createStatement();
 
